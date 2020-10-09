d411ac2067d54438eb86ff1c5c9e93c5ef36c2d3
Zoe Hall <znexfh@gmail.com>
2020-10-09 12:36:51 -0500

    Board layout using div

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,9 @@
 "use strict";
 
 window.onload =function() {
-    var board = document.getElementById('board')
-    var squares= board.children;
+    let squares= document.querySelectorAll('#board div');
 
     for (let i=0;i<9;i++){
         squares[i].classList.add('square');
     }
-    
 }
