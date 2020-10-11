bb1ba59392cd230d0aa721884ca5a1b6baa46851
Zoe Hall <znexfh@gmail.com>
2020-10-11 16:06:00 -0500

    Xs and Os

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,8 +2,25 @@
 
 window.onload =function() {
     let squares= document.querySelectorAll('#board div');
+    var boardstate=["","","","","","","",""];
 
     for (let i=0;i<9;i++){
         squares[i].classList.add('square');
-    }
+    }   
+    var prev ='O';
+    (document.querySelectorAll('.square')).forEach(function(box,i){
+        box.addEventListener('click',function(e){
+            
+            if(prev=='O'){
+                box.textContent="X";
+                box.classList.add('X');
+                prev='X';
+            }else{
+                box.textContent="O";
+                box.classList.add('O');
+                prev='O';
+            }
+            boardstate[i]=box.textContent;
+        });
+    });
 }
